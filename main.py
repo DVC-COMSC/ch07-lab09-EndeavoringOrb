c5f79824553b2de1307aa3f7a259f63aefa3bf87
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 16:26:10 -0700

    fixed small mistake

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ for i in range(len(names)):
     if len(names[i]) < len(shortest) or shortest == "":
         shortest = names[i]
     if len(names[i]) == len(shortest):
-        if ord(names[i][0]) < len(shortest[0]):
+        if ord(names[i][0]) < ord(shortest[0]):
             shortest = names[i]
     if len(names[i]) > len(longest):
         longest = names[i]
-print(shortest, end = " ")
+print(shortest)
 print(longest)
