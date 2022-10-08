e335ed160929020e0b94c283479c07d66c6b8640
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 16:33:09 -0700

    add ord() to longest check

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 names = list(input().split(" "))
 shortest = ""
 longest = ""
+print(names)
 for i in range(len(names)):
     if len(names[i]) < len(shortest) or shortest == "":
         shortest = names[i]
@@ -9,5 +10,8 @@ for i in range(len(names)):
             shortest = names[i]
     if len(names[i]) > len(longest):
         longest = names[i]
+    if len(names[i]) == len(longest):
+        if ord(names[i][0]) > ord(longest[0]):
+            longest = names[i]
 print(shortest)
 print(longest)
