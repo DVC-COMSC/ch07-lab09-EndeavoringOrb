95fdad0edaf43d1ae0287be9702420db13278d61
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 15:34:11 -0700

    1st commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-# ******************************
-# Make your Code
-# ******************************
+names = list(input().lower().split(" "))
+for i in range(len(names)-1):
+    if len(names[i]) > len(names[i+1]):
+        names[i],names[i+1] = names[i+1],names[i]
+    elif len(names[i]) == len(names[i+1]):
+        for j in range(names[i]):
+            if ord(names[i][j]) > ord(names[i][j]):
+                names[i],names[i+1] = names[i+1],names[i]
+                break
+            elif ord(names[i][j]) == ord(names[i][j]):
+                continue
+            else:
+                break
